f24d11dc771372ee690b1181d3115cb0fda1c61c
Artem Govorov <artem.govorov@gmail.com>
2017-05-05 20:03:40 +1000

    Added async call

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
-it('should report diff', () => {
-  expect({myObj: "A", name: "Apple"}).toEqual({myObj: "A", name: "Aardvark"});
+var jestAsyncDone = function(expects, done) {
+  try {
+    expects();
+    done();
+  } catch (e) {
+    done.fail(e);
+  }
+}
+
+it('should report diff', (done) => {
+  jestAsyncDone(function () {
+    expect({myObj: "A", name: "Apple"}).toEqual({myObj: "A", name: "Aardvark"});
+  }, done)
 })
